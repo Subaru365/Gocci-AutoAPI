f308a4390dbf74b8ffbac5b2e65a78701b4aa118
Markus Wanke <m-wanke@inase-inc.jp>
2015-10-05 11:10:09 +0900

    local moot changes

--- types.py
+++ types.py
@@ -12,12 +12,12 @@
 #      "token":"eyJrfehfhwiehfwhfiw877ef7wf7w......"
 #   }
 #}
-API VERSION 		"version" 		"\d+\.\d+"
-API RESPONSE_CODE 	"code" 			"\d\d\d"
-API REQUESTED_URI 	"uri" 			"/[a-z/]+"
-API IDENTIFIER 		"identifier" 	"[A-Z_]+"
-API MESSAGE 		"message" 		"\w+"
-API PAYLOAD 		"payload" 		"valid_json"
+# API VERSION 		"version" 		"\d+\.\d+"
+# API RESPONSE_CODE 	"code" 			"\d\d\d"
+# API REQUESTED_URI 	"uri" 			"/[a-z/]+"
+# API IDENTIFIER 		"identifier" 	"[A-Z_]+"
+# API MESSAGE 		"message" 		"\w+"
+# API PAYLOAD 		"payload" 		"valid_json"
 
 class URIToken:
 
