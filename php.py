30b38ab2b93fc01cd6f957c94d7e63b8a89c2a0f
subaru365 <subaru.woody@gmail.com>
2015-12-14 12:54:10 +0900

    modified:   php.py

--- php.py
+++ php.py
@@ -7,28 +7,20 @@ def generate(everything):
     res = "<?php\n\n"
 
     uripaths = [(uri.path, uri.normalizedKey()) for uri in everything.uriTokens]
-    # res += generateCodeSwitch("GlobalCode", [e.code for e in everything.globalErrors])
+    res += generateCodeSwitch("GlobalCode", [(e.code, e.msg) for e in everything.globalErrors])
 
 
     for uri in everything.uriTokens:
 
         res += generateGetReqParamsFunction(uri.normalizedKey())
         parameters = [ (para.key, para.regex, para.optional) for para in uri.parameters ]
-        res += generateSetRequestParameter(uri.normalizedKey(), parameters)
-        res += generateSetResponseParameter(uri.normalizedKey(), uri.responses)
-        res += generateCodeSwitch(uri.normalizedKey(), [ (e.code, e.msg) for e in uri.errors ])
-
+        res += generateSetRequestParameter(uri.normalizedKey(), parameters, )
+        #res += generateSetResponseParameter(uri.normalizedKey(), uri.responses)
+        #res += generateCodeSwitch(uri.normalizedKey(), [ (e.code, e.msg) for e in uri.errors ])
 
     # ggg = [ "auth.php": lomgstring1, "get.php": longstring2]
 
     return res
-
-
-def generateGetReqParamsFunction(uripath):
-
-    res  = "$this->setReqParams_{KEY}();\n".format(KEY=uripath)
-    res += "return $this->req_params;\n"
-    return wrapInPublicVoidFunction("getReqParams_"+uripath, res)
 
 
 def generateCodeSwitch(switchname, codes):
@@ -38,9 +30,17 @@ def generateCodeSwitch(switchname, codes):
     for (code, msg) in codes:
         res =  "$this->code = '{CODE}';\n".format(CODE=code)
         res += "$this->message = \"{MSG}\";\n".format(MSG=msg)
-        fnc +=  wrapInPublicVoidFunction("set_"+switchname+code, res)
+        fnc +=  wrapInPublicVoidFunction("set"+switchname+"_"+code, res)
 
     return fnc
+
+
+def generateGetReqParamsFunction(uripath):
+
+    res  = "$this->setReqParams{KEY}();\n".format(KEY=uripath)
+    res += "$this->validationRun();\n"
+    res += "return $this->req_params;\n"
+    return wrapInPublicVoidFunction("getReqParams"+uripath, res)
 
 
 def generateSetRequestParameter(uriname, parameters):
@@ -50,7 +50,8 @@ def generateSetRequestParameter(uriname, parameters):
         res += "    '{KEY}' => Input::get('{KEY}'),\n".format(KEY=key)
     res += ");\n\n"
 
-    res += "$val = $this->val;\n\n"
+    res += "$val = $this->val;\n"
+    res += "$val = Validation::forge('');\n"
     for (key, regex, optional) in parameters:
         res += "$val->add('{KEY}', '{KEY}');\n".format(KEY=key)
         if not optional:
@@ -58,32 +59,32 @@ def generateSetRequestParameter(uriname, parameters):
         res += "$val->add_rule('match_pattern', '/{KEY}/');\n\n".format(KEY=regex)
     res += "$this->val = $val;\n"
 
-    return wrapInPivateVoidFunction("setReq_" + uriname, res)
+    return wrapInPivateVoidFunction("setReqParams" + uriname, res)
 
 
-def generateSetResponseParameter(uriname, responses):
+# def generateSetResponseParameter(uriname, responses):
 
-    def onleaf(leaf):
-        nonlocal payload
-        payload += "$payload['{KEY}'] = ({TYPE})$res_params['{KEY}'];\n".format(KEY=leaf.key, TYPE=leaf.typ)
+#     def onleaf(leaf):
+#         nonlocal payload
+#         payload += "$payload['{KEY}'] = ({TYPE})$res_params['{KEY}'];\n".format(KEY=leaf.key, TYPE=leaf.typ)
 
-    def visitor(className, root):
-        nonlocal payload
-        root.traverse(onleaf)
-        begin   = "$res_params = $this->res_params;\n"
-        end     = "$this->payload = $payload;\n"
-        payload = "" if payload == "" else wrapInPivateVoidFunction("setRes_" + uriname, begin+payload+end)
+#     def visitor(className, root):
+#         nonlocal payload
+#         root.traverse(onleaf)
+#         begin   = "$res_params = $this->res_params;\n"
+#         end     = "$this->payload = $payload;\n"
+#         payload = "" if payload == "" else wrapInPivateVoidFunction("setRes_" + uriname, begin+payload+end)
 
-    payload = ""
-    visitor("Payload", responses)
-    return payload
+#     payload = ""
+#     visitor("Payload", responses)
+#     return payload
 
 
 def wrapInInterface(cname, code):
     return "interface {NAME} {{\n{CODE}}}\n\n".format(NAME=cname, CODE=ident(code))
 
 def wrapInClass(cname, code):
-    return "class {NAME} {{\n{CODE}}}\n\n".format(NAME=cname, CODE=ident(code))
+    return "class {NAME} extends Model\n{{\n    use SingletonTrait;\n\n{CODE}\n}}\n\n".format(NAME=cname, CODE=ident(code))
 
 def wrapInPublicVoidFunction(fname, code):
     return "public function {NAME}()\n{{\n{CODE}}}\n\n\n".format(NAME=fname, CODE=ident(code))
